07bb37b5ea11fad991a4c968488ee410befee69e
a1ec <acsus2@gmail.com>
2016-05-02 00:47:33 +1000

    Tidied some DA detail scraping into a spider method, uncomment shell function for debugging

--- cbury_scrapy/spiders/cbury_spider.py
+++ cbury_scrapy/spiders/cbury_spider.py
@@ -1,58 +1,63 @@
 """ Run under bash like so
-    timenow=`date +%Y%m%d_%H%M%S`; scrapy runspider cbury_spider.py -o cbury-scrape-$timenow.json
+    timenow=`date +%Y%m%d_%H%M%S`;
+    scrapy runspider cbury_spider.py -o ../../scrapedcbury-scrape-$timenow.json
 """
 
 import scrapy
 
 from scrapy.exceptions import CloseSpider
 from cbury_scrapy.items import DA, DA_Person, Person
+from scrapy.shell import inspect_response
 
 def td_text_after(label, response):
     """ retrieves text from first td following a td containing a label e.g.:"""
     return response.xpath("//*[contains(text(), '" + label + "')]/following-sibling::td//text()").extract_first()
 
+
 class CburySpider(scrapy.Spider):
     # scrapy.Spider attributes
     name = "cbury"
+    # required for unicode character replacement of '$' and ',' in est_cost
+    translation_table = dict.fromkeys(map(ord, '$,'), None)
+
     allowed_domains = ["datrack.canterbury.nsw.gov.au"]
     start_urls = [
         "http://datrack.canterbury.nsw.gov.au/cgi/datrack.pl?search=search&sortfield=^metadata.date_lodged", # add &startidx=n if need be
     ]
-
+    
+    items_remaining = 0
     da = DA()        
     da['lga'] = u"Canterbury"
-    # CloseSpider.CLOSESPIDER_ITEMCOUNT = MAX_ITEMS_TO_SCRAPE
-    items_remaining = 0
+    CloseSpider.CLOSESPIDER_ITEMCOUNT = 25
+
+    def get_da_url_addr(self, response):
+        # Select all DA rows on the page
+        for row in response.xpath('//table/tr[@class="datrack_resultrow_odd" or @class="datrack_resultrow_even"]'):
+            # Retrieve DA URL and address from the list page
+            r = scrapy.Selector(text=row.extract(), type="html")
+            self.da['da_no'] = r.xpath('//td[@class="datrack_danumber_cell"]//text()').extract_first()#
+            self.da['house_no'] = r.xpath('//td[@class="datrack_houseno_cell"]//text()').extract_first()#
+            self.da['street'] = r.xpath('//td[@class="datrack_street_cell"]//text()').extract_first()#
+            self.da['town'] = r.xpath('//td[@class="datrack_town_cell"]//text()').extract_first()#
+            self.da['url'] = url = r.xpath('//td[@class="datrack_danumber_cell"]//@href').extract_first()#
             
     def parse(self, response):
         """ Retrieve total number of DAs from initial list page """ 
         # get number of total items
         self.items_remaining = int(response.xpath('//span[@class="datrack_count"]//text()').extract_first().split()[-1])
-        self.logger.info('self.items_remaining = %d', self.items_remaining)
+        self.logger.info('DAs found = %d', self.items_remaining)
         yield scrapy.Request(response.url, self.parse_da_list)
     
     def parse_da_list(self, response):
         """ Retrieve DA information from the list page """
-        # Select all DA rows on the page
-        for row in response.xpath('//table/tr[@class="datrack_resultrow_odd" or @class="datrack_resultrow_even"]'):
-            # Retrieve DA URL and address from the list page                
-            r = scrapy.Selector(text=row.extract(), type="html")
-            self.da['da_no'] = r.xpath('//td[@class="datrack_danumber_cell"]//text()').extract_first()
-            self.da['house_no'] = r.xpath('//td[@class="datrack_houseno_cell"]//text()').extract_first()
-            self.da['street'] = r.xpath('//td[@class="datrack_street_cell"]//text()').extract_first()
-            self.da['town'] = r.xpath('//td[@class="datrack_town_cell"]//text()').extract_first()
-            self.da['url'] = url = r.xpath('//td[@class="datrack_danumber_cell"]//@href').extract_first()
-            
-            #yield self.da
-            # get the remaining DA details from its page
-            yield scrapy.Request(url, callback=self.parse_da_item)
-    
-            self.items_remaining -= 1
-            self.logger.info('self.items_remaining = %d', self.items_remaining)
+        # get url and da
+        self.get_da_url_addr(response)
         
-            # terminate crawl if
-            if self.items_remaining <= 0:
-                raise CloseSpider("Scraped required number of items.")
+        # get the remaining DA details from its page
+        yield scrapy.Request(self.da['url'], callback=self.parse_da_item)
+
+        # invoke shell for inspection    
+        # inspect_response(response, self)
                 
         # follow next page link
         next_page = response.xpath("//*[contains(text(), 'Next')]/@href").extract_first()
@@ -66,7 +71,7 @@ class CburySpider(scrapy.Spider):
         labels = { 'date_lodged': 'Date Lodged:', 'desc_full': 'Description:', 
                    'est_cost': 'Estimated Cost:', 'status': 'Status:',
                    'date_determined': 'Date Determined:', 'decision': 'Decision:',
-                   'officer': 'Responsible Officer:'}
+                   'officer': 'Responsible Officer:' }
         
         # map DA fields with those in the folliwng <td> elements on the page
         for i in labels:
@@ -74,7 +79,8 @@ class CburySpider(scrapy.Spider):
 
         # convert est_cost text to int for easier sheet import
         # e.g. "12,000,000" -> 12000000
-        self.da[est_cost] = int(self.da[est_cost].translate(None, '$,'))
+        if self.da['est_cost'] != None:
+            self.da['est_cost'] = int(self.da['est_cost'].translate(self.translation_table))
 
         # Get people data from 'Names' table, 'Role' heading
         self.da['names'] = []
@@ -84,5 +90,5 @@ class CburySpider(scrapy.Spider):
             da_name['name_no'] = row.xpath('normalize-space(./td[2])').extract_first()
             da_name['full_name'] = row.xpath('normalize-space(./td[3])').extract_first()
             self.da['names'].append(da_name)
-                    
+        
         yield self.da
